ab8d9ca83288d5bf93c4c94081d580f99043d194
Strong <dstrongbusiness@gmail.com>
2026-08-08 18:22:34 -0600

    Delete build/generated directory

--- build/generated/switch/config.mk
+++
@@ -1,7 +0,0 @@
-LOAD_KIND := Module
-PROGRAM_ID := 01007ef00011e000
-NPDM_JSON := config.json
-PYTHON := python3
-MOUNT_PATH := /mnt/sdcard
-C_FLAGS := -I"C:/Users/dylan/repos/WiiXLaunch/include" -I"C:/Users/dylan/repos/WiiXLaunch/build/generated/include"
-CXX_FLAGS := -I"C:/Users/dylan/repos/WiiXLaunch/include" -I"C:/Users/dylan/repos/WiiXLaunch/build/generated/include"
